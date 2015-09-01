92f225fa476941d845863c6296d3dff7146fed77
colorMeCode <good-vibrations@hotmail.com>
2015-08-31 20:04:21 -0700

    Create pluck.js

---
+++ pluck.js
@@ -0,0 +1,32 @@
+//pluck_.pluck(list, propertyName) 
+//A convenient version of what is perhaps the most common use-case for map: extracting a list of property values.
+
+var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+_.pluck(stooges, 'name');
+=> ["moe", "larry", "curly"]
+
+//Extract a property within an object from an array
+function pluck(list, prop) {
+    var coop = [];
+        for (var i = 0; i < list.length; i += 1) {
+            //access value with [] to avoid stringifying key property
+            coop.push(list[i][prop])
+        };
+    return coop;
+}
+
+
+//Unnecessary nested loop variant. Ignore this case..
+function pluck(list, prop) {
+    var coop = [];
+    for (var i = 0; i < list.length; i += 1) {
+        for (var key in list[i]) {
+            console.log(list[i][key]);
+            if (key === prop) {
+                coop.push(list[i][key]); 
+            };
+        };
+    };
+   return coop;
+}
+
